aef8061b305bf36bfa99b6123080def93623ea27
Ricardo Gonçalves <ricar.goncalves@gmail.com>
2022-07-28 02:41:08 +0000

    Overall implementation added

--- disk-benchmark.c
+++ disk-benchmark.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
 #include <time.h>
 #include <limits.h>
 
-#define BLOCK_SIZE	4 * 1024 * 1024		// 4M
-#define N_BLOCKS	64					// 256MB (64 * 4)
-#define TIME		15 * 100000			// 15s
-#define DATA_TYPE	uint64
+#define DEFAULT_BLOCK_SIZE	4 * 1024 * 1024		// 4M
+#define DEFAULT_TIME_LIMIT	15					// 15s
 
 typedef unsigned int uint;
 typedef unsigned long long int uint64;
@@ -18,6 +17,7 @@ typedef unsigned short int uint16;
 
 typedef enum {
 	READ,
+	WRITE,
 	READ_WRITE,
 	WRITE_READ,
 	READ_WRITE_READ
@@ -36,6 +36,10 @@ time_t time_now() {
 	clock_gettime(CLOCK_REALTIME, &now);
 	// convert to milliseconds
 	return now.tv_sec * 1000 + now.tv_nsec / 1000000;
+}
+
+int parse_block_size(const char *a) {
+	return DEFAULT_BLOCK_SIZE;
 }
 
 void fill_buffer(char *buffer, size_t size) {
@@ -77,30 +81,30 @@ void print_partial(TEST *test, bool is_write) {
 
 void print_total(TEST *test, bool is_write) {
 	char str[20];
-	char *msg = (!is_write ?  "Read" : "Write");
-	printf("Bytes %s: %lluMB (%llu) in %.2fs, %d blocks\n", msg,
+	char *type = (!is_write ?  "Read" : "Write");
+	printf("Bytes %s: %lluMB (%llu) in %.2fs, %d blocks\n", type,
 		test->bytes_total / 1024 / 1024,
 		test->bytes_total,
 		test->time_total / (float) 1000,
 		test->count_op);
-	printf("%s speed: %s\n", msg,
+	printf("%s speed: %s\n", type,
 		format_speed(str, test->bytes_total, test->time_total));
 }
 
-int benchmark_operation(int fd, TEST *test, char *buffer, bool write_enabled) {
+ssize_t benchmark_operation(int fd, TEST *test, char *buffer, size_t block_size, bool is_write) {
 	time_t start, end;
 	ssize_t result;
 
 	start = time_now();
-	result = write_enabled ?	// Is read or write operations
-		write(fd, buffer, BLOCK_SIZE) :
-		read(fd, buffer, BLOCK_SIZE);
+	result = is_write ?	// Is read or write operations
+		write(fd, buffer, block_size) :
+		read(fd, buffer, block_size);
 	end = time_now();
 	
 	if(result < 1) {
-		print_partial(test, write_enabled);
+		print_partial(test, is_write);
 		fprintf(stderr, "I/O operation error with errno %d\n", errno);
-		return 1;
+		return -1;
 	}
 
 	time_t time_elapsed = end - start;
@@ -123,85 +127,175 @@ int benchmark_operation(int fd, TEST *test, char *buffer, bool write_enabled) {
 	// 	return 1;
 	// }
 
-	print_partial(test, write_enabled);
+	print_partial(test, is_write);
 
-	return 0;
+	return result;
 }
 
-void benchmark_file(char *filename, TEST_TYPE test_type) {
-	char buffer[BLOCK_SIZE];
+void benchmark_file(const char *file_name, TEST_TYPE test_type, size_t block_size, time_t time_limit, bool is_random) {
+	char buffer[block_size];
 	TEST t1, t2, t3;
 	time_t time_total;
+	off_t result;
 
 	// Select flag for READ-ONLY or READ-WRITE
-	int open_mode = (
-		test_type == READ_WRITE ||
-		test_type == WRITE_READ ||
-		test_type == READ_WRITE_READ) ?
-		O_RDWR : O_RDONLY;
+	int open_mode = (test_type != READ ? O_RDONLY : O_RDWR);
 
 	// Open file
-	int fd = open(filename, open_mode | /* O_DIRECT | */ O_SYNC | O_CREAT, S_IRUSR | S_IWUSR);
+	int fd = open(file_name, open_mode | /* O_DIRECT | */ O_SYNC | O_CREAT, S_IRUSR | S_IWUSR);
 	if(fd < 0) {
-		fprintf(stderr, "Cannot open %s, errno %d\n", filename, errno);
+		fprintf(stderr, "Cannot open %s, errno %d\n", file_name, errno);
 		exit(EXIT_FAILURE);
 	}
 
 	int err = 0;
-	printf("Reading contents of %s file...\n", filename);
+	printf("Running benchmark on file %s...\n", file_name);
 	do {
+		printf("%d\n", test_type);
 		switch (test_type) {
 			case READ_WRITE_READ:
 			case READ_WRITE:
+				printf("READ_WRITE_READ\n");
 				// Read
-				benchmark_operation(fd, &t1, buffer, false);
+				result = benchmark_operation(fd, &t1, buffer, block_size, false);
+				lseek(fd, result, SEEK_CUR);
 			case WRITE_READ:
-				// Generate data for WRITE_READ mode
-				if(test_type == WRITE_READ)
-					fill_buffer(buffer, BLOCK_SIZE);
+			case WRITE:
+				printf("WRITE_READ\n");
+				// Generate data for WRITE_READ and WRITE modes
+				if(test_type == WRITE_READ || test_type == WRITE)
+					fill_buffer(buffer, block_size);
 				// Write
-				benchmark_operation(fd, &t2, buffer, true);
-				// Do not read back with READ_WRITE
-				if(test_type == READ_WRITE)
+				result = benchmark_operation(fd, &t2, buffer, block_size, true);
+				printf("%ld\n", result);
+				// Do not read back with READ_WRITE and WRITE modes
+				if(test_type == READ_WRITE || test_type == WRITE)
 					break;
+					
+				lseek(fd, result, SEEK_CUR);
 			case READ:
-				err = benchmark_operation(fd, &t3, buffer, false);
+				printf("READ\n");
+				err = benchmark_operation(fd, &t3, buffer, block_size, false);
 				break;
 			default:
 				perror("Invalid operation!\n");
 				exit(EXIT_FAILURE);
 		}
 		time_total = (t1.time_total + t2.time_total + t3.time_total) / 1000;
-	} while(time_total < 15 && err == 0);	// 15s
+	} while(time_total < time_limit && err == 0);	// Terminate test
 
 	close(fd);
 
-	print_total(&t1, false);
-	print_total(&t2, true);
-	print_total(&t3, false);
+	// Print results
+	switch (test_type) {
+		case READ_WRITE_READ:
+		case READ_WRITE:
+			print_total(&t1, false);
+		case WRITE_READ:
+		case WRITE:
+			print_total(&t2, true);
+			if(test_type == READ_WRITE || test_type == WRITE)
+				break;
+		case READ:
+			print_total(&t3, false);
+	}
 }
 
-	// do {
-	// } while(read_time < TIME && !feof(fp));
-
+void help() {
+// Usage of server/photo-gallery:
+//   -b, --cache-thumbnails         Generate thumbnails in background when the application starts
+//   -c, --collection stringArray   Specify a new collection. Example name=Default,path=/photos,thumbs=/thumbs
+//       --disable-webdav           Disable WebDAV
+	puts("Usage of disk-benchmark:");
+	puts("  -r                 Read test");
+	puts("  -w                 Write test. The data written is randomly generated, this is a DESTRUCTIVE test");
+	puts("  -rw                Read and write test. The data written is the data read");
+	puts("  -wr                Write and read test. It verifies if the write operation completed correcly. The data written is randomly generated, this is a DESTRUCTIVE test");
+	puts("  -rwr               Read, write and read test. It verifies if the write operation completed correcly. The data written is the data read");
+	puts("  -h, --help         Shows this help");
+	puts("  -s, --sequential   Performs a sequential test");
+	puts("  -u, --random       Performs a random test, a 4K block size will be used");
+	puts("  -b, --block-size   Sets the block size for sequential test");
+	puts("  -t, --time-limit   Duration of each test");
+	exit(EXIT_FAILURE);
+}
 
 int main(int argc, char **argv) {
-	
+	TEST_TYPE test_type = READ;
+	char *file_name = NULL;
+	bool sequential_test = false;
+	bool random_test = false;
+	size_t block_size = DEFAULT_BLOCK_SIZE;
+	time_t time_limit = DEFAULT_TIME_LIMIT;
 	int i;
-
-	for(i=0; i<argc; i++) {
-
-
-	}
-
-	
-	if(argc != 2) {
-		perror("No filename provided\n");
-		exit(EXIT_FAILURE);
-	}
-	char *file_name = argv[1];
-
+	// initialize random seed
 	srand(time(NULL));
 
-	benchmark_file(file_name, READ);
+	for(i=1; i<argc; i++) {
+		char *a = argv[i];
+		// Mode selection
+		if(strcmp("-r", a) == 0)
+			test_type = READ;
+		else if(strcmp("-w", a) == 0)
+			test_type = WRITE;
+		else if(strcmp("-rw", a) == 0)
+			test_type = READ_WRITE;
+		else if(strcmp("-wr", a) == 0)
+			test_type = WRITE_READ;
+		else if(strcmp("-rwr", a) == 0)
+			test_type = READ_WRITE_READ;
+		// Sequencial or random test
+		else if(strcmp("-s", a) == 0 || strcmp("--sequential", a) == 0)
+			sequential_test = true;
+		else if(strcmp("-u", a) == 0 || strcmp("--random", a) == 0)
+			random_test = true;
+		// Block size and time limit
+		else if(strcmp("-b", a) == 0 || strcmp("--block-size", a) == 0) {
+			i++;
+			block_size = parse_block_size(argv[i]);
+		}
+		else if(strcmp("-t", a) == 0 || strcmp("--time-limit", a) == 0) {
+			i++;
+			time_limit = atoi(argv[i]);
+		}
+		else if(strcmp("-h", a) == 0 || strcmp("--help", a) == 0)
+			help();
+		else {
+			if(strncmp("-", a, 1)==0) {	// Option not recognized
+				fprintf(stderr, "Option %s not recognized\n", a);
+				help();
+			}
+			file_name = a;
+		}
+	}
+
+	if(file_name == NULL) {
+		perror("No filename provided\n");
+		help();
+	}
+	
+	// Data loss checks
+	if(test_type != READ) {
+		char ans[20];
+		puts("The selected test will perform writing operation on the file.");
+		puts("Data LOSS can occur. Are you sure you want to continue? (yes/no)");
+		fgets(ans, 20, stdin);
+		if(strcmp("yes\n", ans) != 0)
+			exit(EXIT_FAILURE);
+	}
+	if(test_type == WRITE || test_type == WRITE_READ) {
+		char ans[20];
+		puts("Random data will be written over your existing data.");
+		puts("This action is NOT RECOVERABLE. Are you really sure you want to continue? (yes/no)");
+		fgets(ans, 20, stdin);
+		if(strcmp("yes\n", ans) != 0)
+			exit(EXIT_FAILURE);
+	}
+	
+	if(sequential_test || sequential_test == false && random_test == false)		// Default option
+		benchmark_file(file_name, test_type, block_size, time_limit, false);
+	if(random_test)
+		benchmark_file(file_name, test_type, 4 * 1024, time_limit, true);		// 4K (i.e. 4k random)
+	
+	exit(EXIT_SUCCESS);
 }
